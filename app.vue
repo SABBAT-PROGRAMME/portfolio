804423ec4488511b22892deb34c8669cc1be47ec
= <=>
2025-02-27 21:05:10 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- <Navbar /> -->
+    <NuxtPage />
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import Navbar from "./components/Navbar.vue";
+</script>
+
+<style></style>
